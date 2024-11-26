737658de18ae90f92025bd1d452c4e46333344ed
FelipeMDB <>
2024-11-26 17:22:30 -0300

    Create auxiliares.py
    arquivo para colocar códigos auxiliares

---
+++ auxiliares.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    # if len(sys.argv) != 5:
+    #     print("Uso: python service.py <S> <C> <P> <A>")
+    #     sys.exit(1)
+
+    # S = int(sys.argv[1])
+    # C = int(sys.argv[2])
+    # P = int(sys.argv[3])
+    # A = int(sys.argv[4])
